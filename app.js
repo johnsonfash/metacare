0d8ea20453f41b0b3cf02a8c6cd2dae1217102ad
johnsonfash <tosin.fashanu@supermartng.com>
2022-01-25 10:39:38 +0100

    .

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-// const getMovies = require('./controllers/getMovies');
-// const getComments = require('./controllers/getComments');
-// const { mysql } = require('./models/mysql');
-// const addComment = require('./controllers/addComment');
-// const characterController = require('./controllers/characterController');
-// const getIP = require('./middlewares/getIP');
+const getMovies = require('./controllers/getMovies');
+const getComments = require('./controllers/getComments');
+const { mysql } = require('./models/mysql');
+const addComment = require('./controllers/addComment');
+const characterController = require('./controllers/characterController');
+const getIP = require('./middlewares/getIP');
 const app = express();
 
 app.use(express.json());
@@ -13,12 +13,13 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 })
 
-// app.get('/movies', getMovies);
-// app.get('/movies/:id/comments', getComments);
-// app.post('/movies/:id/comments', getIP, addComment);
-// app.use('/movies/:id/characters', characterController);
+app.get('/movies', getMovies);
+app.get('/movies/:id/comments', getComments);
+app.post('/movies/:id/comments', getIP, addComment);
+app.use('/movies/:id/characters', characterController);
 
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
   // mysql.connect();
   console.log(`Server started at port : ${port}`)
